b878b00f9a7c7e3eee00c8be84a6e2081fc6a39e
chenshanlin001 <chenshanlin001@gmail.com>
2026-01-05 15:42:05 +0800

    update link

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,10 +2,10 @@
   <footer class="corner-footer">
     <span class="footer-copy">Copyright 2025 NavTalk. All rights reserved.</span>
     <div class="footer-links">
-      <a href="https://www.youtube.com/@navtalk" target="_blank" rel="noreferrer">YouTube</a>
-      <a href="https://discord.gg/navtalk" target="_blank" rel="noreferrer">Discord</a>
-      <a href="https://x.com/navtalkai" target="_blank" rel="noreferrer">X</a>
-      <a href="https://www.linkedin.com/company/navtalk-ai" target="_blank" rel="noreferrer">LinkedIn</a>
+      <a href="https://www.youtube.com/@frankfu007" target="_blank" rel="noreferrer">YouTube</a>
+      <a href="https://discord.com/invite/A9VE3zXM9p" target="_blank" rel="noreferrer">Discord</a>
+      <a href="https://x.com/NavTalkAI" target="_blank" rel="noreferrer">X</a>
+      <a href="https://www.linkedin.com/company/navtalk/" target="_blank" rel="noreferrer">LinkedIn</a>
     </div>
   </footer>
 </template>
